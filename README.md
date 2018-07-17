31480fa0cb4b0ad591fff6b2e7a60f029f1d0f48
Metta Ong <ongspxm@gmail.com>
2018-07-17 17:09:38 +0800

    init commit
    commit 7b78d8b66995251b4bc09eda50a33a3b4e7ef337
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Tue Jul 17 17:06:52 2018 +0800
    
        add read me
    
    commit f1891e2b0c77e963826a85c1a97c097da34bec8a
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Tue Jul 17 16:57:31 2018 +0800
    
        done, ready to be pushed
    
    commit 3a302604f86d521dec6044d9554032f71cd5ef00
    Author: Metta Ong <ongspxm@gmail.com>
    Date:   Tue Jul 17 12:51:23 2018 +0800
    
        wip: added in pug, havent write to output yet

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spuild: Web build tool in python
+
+This build tool automates the web development build process. Well, there is gulp and there is grunt, which are much more feature-packed. But in a limited development environment where there is little permission, **spuild** is the tool for you.
+
+Currently, these are plugins that are ready for use:
+
+- Generating HTML from JADE templates
+- Generating CSS form SCSS templates
+
+## to use
+`npm install spuild`
---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const glob = require('glob');
+
+const pug = require('pug');
+const sass = require('sass');
+
+const buildPath = 'build';
+const srcPath = process.argv[2] || 'src';
+const getPath = path => `${buildPath}${path.slice(3)}`;
+
+if (!fs.existsSync(buildPath)) {
+  fs.mkdirSync(buildPath);
+}
+
+const prettyPrint = false;
+
+// generating the output //
+
+function scss(fname) {
+  return sass.renderSync({
+    file: fname,
+    sourceMap: false,
+    sourceComments: false,
+    outputStyle: prettyPrint ? 'nested' : 'compressed',
+  }).css;
+}
+
+function jade(fname) {
+  return pug.renderFile(fname, {
+    pretty: prettyPrint,
+  });
+}
+
+function copy(fname) {
+  return fs.readFileSync(fname);
+}
+
+const funcs = {
+  pug: jade,
+  jade,
+  scss,
+  sass: scss,
+};
+
+// formats stuff //
+
+function swapFormat(fext) {
+  return (path) => {
+    const fname = path.split('.').slice(0, -1).join('.');
+    return `${fname}.${fext}`;
+  };
+}
+
+function identity(fext) {
+  return fext;
+}
+
+const formats = {
+  pug: swapFormat('html'),
+  jade: swapFormat('html'),
+  scss: swapFormat('css'),
+  sass: swapFormat('css'),
+};
+
+glob(`${srcPath}/**/*`, (err, files) => {
+  files.forEach((fname) => {
+    if (fs.lstatSync(fname).isDirectory()) {
+      const dname = getPath(fname);
+      if (!fs.existsSync(dname)) {
+        fs.mkdirSync(dname);
+      }
+    } else {
+      console.log(fname);
+
+      const fext = fname.split('.').slice(-1)[0];
+      const funcOutput = funcs[fext] || copy;
+      const funcFormat = formats[fext] || identity;
+
+      const fname2 = funcFormat(getPath(fname));
+      fs.writeFileSync(fname2, funcOutput(fname));
+    }
+  });
+});
